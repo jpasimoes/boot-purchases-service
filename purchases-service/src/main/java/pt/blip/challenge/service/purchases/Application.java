cd7f83762366c0998e3d34665a19e28472b5a794
joao.simoes <joao.simoes@talkdesk.com>
2016-06-15 00:56:41 +0100

    Bootstrap project

---
+++ purchases-service/src/main/java/pt/blip/challenge/service/purchases/Application.java
@@ -0,0 +1,18 @@
+package pt.blip.challenge.service.purchases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The boot application class that defines the spring boot application.<br>
+ * <br>
+ *
+ * @author jpasimoes
+ *
+ */
+@SpringBootApplication
+public class Application {
+    public static void main( String[] args ) {
+        SpringApplication.run(Application.class, args);
+    }
+}
